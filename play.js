3593ba242010f9933d18d0508f5b71ebf3f74154
Sam Maisha <sam.maisha99@gmail.com>
2022-12-13 17:23:39 +0000

    modularized code for handling user input

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 // connect function from client.js file
-const connect = require("./client.js");
-
+const { connect } = require("./client.js");
+const { setupInput } = require("./input")
 
 console.log("Connecting...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-
-  //event listener for stdin
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-}
-
-// callback function to handle user input
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
 setupInput();

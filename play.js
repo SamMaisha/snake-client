04fed47e8875fc3539b701acb9983134d979bbf3
Sam Maisha <sam.maisha99@gmail.com>
2022-12-13 12:38:40 +0000

    made code more modular

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const net = require("net");
+const connect = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541,
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (messageFromServer) => {
-    console.log(`message from server: ${messageFromServer}`);
-  })
-
-  return conn;
-
-};
 
 console.log("Connecting...");
-connect()
+connect();
